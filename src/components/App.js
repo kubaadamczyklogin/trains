765163b3d14afb53c051871443749cf6a6731306
[kuba] <[poczta@kubaadamczyk]>
2022-04-09 17:57:40 +0200

    Apolla component Query and App as Class Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
 import { addingMinutes, millisecondsToTime } from "./../functions";
 import { TrainsToWorkTable } from "./TrainsToWorkTable";
 import { TrainsHomeTable } from "./TrainsHomeTable";
@@ -7,179 +6,185 @@ import { CustomTime } from "./CustomTime";
 import { Results } from "./Results";
 import "./App.css";
 
-const gqlQuote = gql`
-  query getQuote {    
-    ByBikeToTrain {     
-      FromHomeToTorMia
-      FromHomeToTorGlo
-      FromTrainToWork
-      FromWorkToTrain
-      FromTorMiaToHome
-      FromTorGloToHome    
-    } 
-    TrainsToTorun{ 
-      TorMia
-      TorGlo
-      BydBie     
-    }   
-    TrainsToBydgoszcz{ 
-      TorMia
-      TorGlo
-      BydBie     
-    } 
-  }
-`;
+export class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.gqlData = props.gqlData;   
+    this.byBikeToTrain = this.gqlData.ByBikeToTrain;
+    this.trainsToTorun = this.gqlData.TrainsToTorun; 
+    this.trainsToBydgoszcz = this.gqlData.TrainsToBydgoszcz; 
 
-export function App() {
-  const { data, loading, error} = useQuery(gqlQuote);  
-  const [leaveHome, setLeaveHome] = useState("?");
-  const [aboutTrainToWork, setAboutTrainToWork] = useState("?");
-  const [selectedTrainToWork, setSelectedTrainToWork] = useState("?");
-  const [afterEightHuers, setAfterEightHuers] = useState("?");
-  const [suggestedTrains, setSuggestedTrains] = useState([]);
-  const [startWork, setStartWork] = useState("?");
-  const [workTime, setWorkTime] = useState("?");
-  const [overTime, setOverTime] = useState("?");
-  const [leaveWork, setLeaveWork] = useState("?");
-  const [aboutTrainHome, setAboutTrainHome] = useState("?");
-  const [selectedTrainHome, setSelectedTrainHome] = useState("?");
-  const [comingHome, setComingHome] = useState("?");
-
-  if (loading) {   
-    return(
-      <div className="App">Loading...</div>
-    )
-  } else if(error) {    
-    return(
-      <div className="App">Nie udao sié wczyta danych</div>
-    )
-  }  else { 
-    const byBikeToTrain = data.ByBikeToTrain;
-    const trainsToTorun = data.TrainsToTorun; 
-    const trainsToBydgoszcz = data.TrainsToBydgoszcz;
-
-    
-
-    function findSugestedTrains(workFinishTime) {
-      let suggestedEarly = ["?", -10000000];
-      let suggestedLate = ["?", 10000000];
-      let idealTrainTime = addingMinutes(
-        workFinishTime,
-        byBikeToTrain.FromWorkToTrain
-      );
-      idealTrainTime = idealTrainTime.split(":");
-
-      let time = new Date();
-      const idealTrainTimeRaw = time.setHours(
-        idealTrainTime[0],
-        idealTrainTime[1]
-      );
-
-      trainsToTorun.forEach((train) => {
-        let trainTimeRaw = train.BydBie.split(":");
-        time = new Date();
-        trainTimeRaw = time.setHours(trainTimeRaw[0], trainTimeRaw[1]);
-        const result = trainTimeRaw - idealTrainTimeRaw;
-
-        if (result > 0) {
-          if (suggestedLate[1] > result) suggestedLate = [train.BydBie, result];
-        } else {
-          if (suggestedEarly[1] < result) suggestedEarly = [train.BydBie, result];
-        }
-      });
-
-      setSuggestedTrains([suggestedEarly[0], suggestedLate[0]]);
+    this.state = {
+      leaveHome: "?",
+      aboutTrainToWork: "?",
+      selectedTrainToWork: "?",
+      afterEightHuers: "?",
+      suggestedTrains: [],
+      startWork: "?",
+      workTime: "?",
+      overTime: "?",
+      leaveWork: "?",
+      aboutTrainHome: "?",
+      selectedTrainHome: "?",
+      comingHome: "?"
     }
+  }    
 
-    function howMuchHouers(startWork, endWork) {
-      if (startWork !== "?" && endWork !== "?") {
-        startWork = startWork.split(":");
-        let time = new Date();
-        const startWorkRaw = time.setHours(startWork[0], startWork[1]);
-        endWork = endWork.split(":");
-        time = new Date();
-        const endWorkRaw = time.setHours(endWork[0], endWork[1]);
-        const totalWorkTime = endWorkRaw - startWorkRaw;
-        const overTime = totalWorkTime - 8 * 60 * 60 * 1000;
-        setWorkTime(millisecondsToTime(totalWorkTime));
-        setOverTime(millisecondsToTime(overTime));
+  findSugestedTrains(workFinishTime) {    
+    let suggestedEarly = ["?", -10000000];
+    let suggestedLate = ["?", 10000000];
+    let idealTrainTime = addingMinutes(
+      workFinishTime,
+      this.byBikeToTrain.FromWorkToTrain
+    );
+    idealTrainTime = idealTrainTime.split(":");
+
+    let time = new Date();
+    const idealTrainTimeRaw = time.setHours(
+      idealTrainTime[0],
+      idealTrainTime[1]
+    );
+
+    this.trainsToTorun.forEach((train) => {
+      let trainTimeRaw = train.BydBie.split(":");
+      time = new Date();
+      trainTimeRaw = time.setHours(trainTimeRaw[0], trainTimeRaw[1]);
+      const result = trainTimeRaw - idealTrainTimeRaw;
+
+      if (result > 0) {
+        if (suggestedLate[1] > result) suggestedLate = [train.BydBie, result];
+      } else {
+        if (suggestedEarly[1] < result) suggestedEarly = [train.BydBie, result];
       }
-    }
+    });
 
-    function handleSelectTrainToWork(home, TorGlo, aboutTrain, work) {
+    this.setState({
+      suggestedTrains: [suggestedEarly[0], suggestedLate[0]]
+    });
+  }
+
+  howMuchHouers(startWork, endWork) {   
+    if (startWork !== "?" && endWork !== "?") {
+      startWork = startWork.split(":");
+      let time = new Date();
+      const startWorkRaw = time.setHours(startWork[0], startWork[1]);
+      endWork = endWork.split(":");
+      time = new Date();
+      const endWorkRaw = time.setHours(endWork[0], endWork[1]);
+      const totalWorkTime = endWorkRaw - startWorkRaw;
+      const overTime = totalWorkTime - 8 * 60 * 60 * 1000;
+
+      this.setState({
+        workTime: millisecondsToTime(totalWorkTime),
+        overTime: millisecondsToTime(overTime)
+      });    
+    }
+  }
+
+  handleSelectTrainToWork(home, TorGlo, aboutTrain, work) {    
       const workPlusEight = addingMinutes(work, 8 * 60);
 
-      setLeaveHome(home);
-      setStartWork(work);
-      setAboutTrainToWork(aboutTrain);
-      setSelectedTrainToWork(TorGlo);
-      setAfterEightHuers(workPlusEight);
-      findSugestedTrains(workPlusEight);
-      howMuchHouers(work, leaveWork);
-    }
+      this.setState({
+        leaveHome: home,
+        startWork: work,
+        aboutTrainToWork: aboutTrain,
+        selectedTrainToWork: TorGlo,
+        afterEightHuers: workPlusEight
+      });
 
-    function handleSelectTrainHome(work, BydBie, aboutTrain, home) {
-      setLeaveWork(work);
-      setSelectedTrainHome(BydBie);
-      setAboutTrainHome(aboutTrain);
-      setComingHome(home);
-      howMuchHouers(startWork, work);
-    }
-
-    function handleTimeChange(timeToChange, timeType, modifier) {
-      const timeAfterChange = addingMinutes(timeToChange, modifier);
-
-      if (timeType === "startWork") {
-        const workPlusEight = addingMinutes(timeAfterChange, 8 * 60);
-        setStartWork(timeAfterChange);
-        setAfterEightHuers(workPlusEight);
-        findSugestedTrains(workPlusEight);
-        howMuchHouers(timeAfterChange, leaveWork);
-      } else if (timeType === "leaveWork") {
-        setLeaveWork(timeAfterChange);
-        howMuchHouers(startWork, timeAfterChange);
-      }
-    }
-    return (
-      <div className="App">
-        <div className="TransportEditor">               
-          <TrainsToWorkTable
-            trainsToBydgoszcz={trainsToBydgoszcz}
-            byBikeToTrain={byBikeToTrain}
-            SelectTrainToWork={handleSelectTrainToWork}
-            selectedTrain={selectedTrainToWork}
-          />
-          <CustomTime
-            timeToChange={startWork}
-            timeType="startWork"
-            TimeChange={handleTimeChange}
-          />
-          <CustomTime
-            timeToChange={leaveWork}
-            timeType="leaveWork"
-            TimeChange={handleTimeChange}
-          />
-          <TrainsHomeTable
-            trainsToTorun={trainsToTorun}
-            byBikeToTrain={byBikeToTrain}
-            SelectTrainHome={handleSelectTrainHome}
-            selectedTrain={selectedTrainHome}
-            suggestedTrains={suggestedTrains}
-          />
-        </div>
-        <Results
-          leaveHome={leaveHome}
-          leaveWork={leaveWork}
-          startWork={startWork}
-          aboutTrainToWork={aboutTrainToWork}
-          afterEightHuers={afterEightHuers}
-          workTime={workTime}
-          overTime={overTime}
-          aboutTrainHome={aboutTrainHome}
-          comingHome={comingHome}
-        />
-      </div>
-    );
+      this.findSugestedTrains(workPlusEight);
+      this.howMuchHouers(work, this.state.leaveWork);
   }
-}
 
+  handleSelectTrainHome(work, BydBie, aboutTrain, home) {  
+    
+    this.setState({
+      leaveWork: work,
+      selectedTrainHome: BydBie,
+      aboutTrainHome: aboutTrain,
+      comingHome: home
+    });    
+
+    this.howMuchHouers(this.state.startWork, work);
+  }
+
+  handleTimeChange(timeToChange, timeType, modifier) {    
+    const timeAfterChange = addingMinutes(timeToChange, modifier);
+
+    if (timeType === "startWork") {
+      const workPlusEight = addingMinutes(timeAfterChange, 8 * 60);
+
+      this.setState({
+        startWork: timeAfterChange,
+        afterEightHuers: workPlusEight
+      });      
+
+      this.findSugestedTrains(workPlusEight);
+      this.howMuchHouers(timeAfterChange, this.state.leaveWork);
+
+    } else if (timeType === "leaveWork") {
+      this.setState({
+        leaveWork: timeAfterChange
+      });     
+
+      this.howMuchHouers(this.state.startWork, timeAfterChange);
+    }
+  }
+
+  render() { 
+    const {
+      leaveHome,
+      aboutTrainToWork,
+      selectedTrainToWork,
+      afterEightHuers,
+      suggestedTrains,
+      startWork,
+      workTime,
+      overTime,
+      leaveWork,
+      aboutTrainHome,
+      selectedTrainHome,
+      comingHome
+    } = this.state;
+      return (
+        <div className="App">          
+          <div className="TransportEditor">               
+            <TrainsToWorkTable
+              trainsToBydgoszcz={this.trainsToBydgoszcz}
+              byBikeToTrain={this.byBikeToTrain}
+              SelectTrainToWork={this.handleSelectTrainToWork.bind(this)}
+              selectedTrain={selectedTrainToWork}
+            />
+            <CustomTime
+              timeToChange={startWork}
+              timeType="startWork"
+              TimeChange={this.handleTimeChange.bind(this)}
+            />
+            <CustomTime
+              timeToChange={leaveWork}
+              timeType="leaveWork"
+              TimeChange={this.handleTimeChange.bind(this)}
+            />
+            <TrainsHomeTable
+              trainsToTorun={this.trainsToTorun}
+              byBikeToTrain={this.byBikeToTrain}
+              SelectTrainHome={this.handleSelectTrainHome.bind(this)}
+              selectedTrain={selectedTrainHome}
+              suggestedTrains={suggestedTrains}
+            />
+          </div>
+          <Results
+            leaveHome={leaveHome}
+            leaveWork={leaveWork}
+            startWork={startWork}
+            aboutTrainToWork={aboutTrainToWork}
+            afterEightHuers={afterEightHuers}
+            workTime={workTime}
+            overTime={overTime}
+            aboutTrainHome={aboutTrainHome}
+            comingHome={comingHome}
+          /> 
+        </div>
+      );     
+  }  
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider} from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider, gql} from '@apollo/client';
+import {Query} from '@apollo/client/react/components';
 import "./styles.css";
 
 import { App } from "./components/App";
@@ -14,11 +15,41 @@ const client = new ApolloClient({
   })
 });
 
+const gqlQuote = gql`
+  query getQuote {    
+    ByBikeToTrain {     
+      FromHomeToTorMia
+      FromHomeToTorGlo
+      FromTrainToWork
+      FromWorkToTrain
+      FromTorMiaToHome
+      FromTorGloToHome    
+    } 
+    TrainsToTorun{ 
+      TorMia
+      TorGlo
+      BydBie     
+    }   
+    TrainsToBydgoszcz{ 
+      TorMia
+      TorGlo
+      BydBie     
+    } 
+  }
+`;
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <Query query={gqlQuote}> 
+        {({ loading, error, data }) => {
+            if (error) return <div className="App">Nie udao sié wczyta danych</div>;
+            if (loading || !data) return <div className="App">Loading...</div>;
+
+            return  <App gqlData={data}/>
+        }}             
+      </Query>           
     </ApolloProvider>  
   </StrictMode>,
   rootElement
